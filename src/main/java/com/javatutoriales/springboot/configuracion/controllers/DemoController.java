2e98b74e3870003b5cf7208a7b88388a56152694
JavaTutoriales <programadorjavablog@gmail.com>
2019-03-12 23:28:50 -0600

    Se lee el valor de la propiedad demo.valorEnv usando el objeto Environment de Spring Boot

--- src/main/java/com/javatutoriales/springboot/configuracion/controllers/DemoController.java
+++ src/main/java/com/javatutoriales/springboot/configuracion/controllers/DemoController.java
@@ -1,6 +1,7 @@
 package com.javatutoriales.springboot.configuracion.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,18 @@ public class DemoController {
 	@Setter (onMethod=@__(@Autowired))
 	private EjemploConfig config;
 	
+	@Setter (onMethod=@__(@Autowired))
+	private Environment env;
+	
 	@GetMapping
 	public String saluda() {
 		return "Hola " + config.getValor();
 	}
+	
+	@GetMapping("/env")
+	public String saludaEnv() {
+		return "Hola " + env.getProperty("demo.valorEnv");
+	}
+	
+	
 }
